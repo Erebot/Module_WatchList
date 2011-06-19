abd67c24839d87907f40de45174c78d4c2e72fad
François Poirotte <clicky@erebot.net>
2011-06-19 06:43:00 +0000

    Add some documentation.

--- src/Erebot/Module/WatchList.php
+++ src/Erebot/Module/WatchList.php
@@ -16,11 +16,34 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * \brief
+ *      A module that can be used to manage the bot's IRC WATCH list.
+ *
+ * This module adds support for the WATCH/ISON commands to Erebot.
+ * It can be used to monitor (dis)connections of some IRC users.
+ *
+ * \attention
+ *      The WATCH command is an extension to the official IRC protocol
+ *      and is supported only by some IRC networks.
+ *      Most IRC servers that implement it have strict restrictions
+ *      on the number of users someone may track using this command.
+ *      The ISON command however is safe (and is part of the official
+ *      IRC protocol).
+ *      If you plan to track a lot of users, don't use this module.
+ *
+ * \note
+ *      In the future, this module may use the most appropriate
+ *      command to monitor (dis)connections, depending on the
+ *      number of nicks it has to track.
+ */
 class   Erebot_Module_WatchList
 extends Erebot_Module_Base
 {
+    /// List of nicknames currently being followed by this module.
     protected $_watchedNicks;
 
+    /// \copydoc Erebot_Module_Base::_reload()
     public function _reload($flags)
     {
         if ($flags & self::RELOAD_HANDLERS) {
@@ -39,10 +62,19 @@ extends Erebot_Module_Base
         }
     }
 
+    /// \copydoc Erebot_Module_Base::_unload()
     protected function _unload()
     {
     }
 
+    /**
+     * Upon connection, requests that the server send notifications
+     * for (dis)connection related to IRC nicknames this modules is
+     * currently following.
+     *
+     * \param Erebot_Interface_Event_Connect $event
+     *      Connection event.
+     */
     public function handleConnect(Erebot_Interface_Event_Connect $event)
     {
         if (!count($this->_watchedNicks))
